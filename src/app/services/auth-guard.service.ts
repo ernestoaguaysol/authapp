83b09ad68be3e83e51f95aaae1d08bdb04b05817
Ernesto <ernestoaguaysol@gmail.com>
2017-12-19 16:38:48 -0300

    protegida en router

--- src/app/services/auth-guard.service.ts
+++ src/app/services/auth-guard.service.ts
@@ -1,8 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Router, ActivatedRouteSnapshot, RouterStateSnapshot, CanActivate } from '@angular/router';
+import { AuthService } from './auth.service';
 
 @Injectable()
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate {
 
-  constructor() { }
+   constructor(private auth: AuthService) { }
 
+  canActivate(next: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+
+    console.log(next);
+
+    if (this.auth.isAuthenticated()) {
+      console.log('paso el guard');
+      return true;
+    }else {
+      console.log('bloqueado');
+      return false;
+    }
+  }
 }
